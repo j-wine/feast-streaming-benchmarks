ac051ba7be79379cc440564cbf92f99b8b41b692
Weinhold, Jacob <jacob.weinhold@stud.th-luebeck.de>
2024-12-11 00:12:47 +0100

    to do:  - use docker apply to create first data files - debug docker compose

--- feature_repo/feature_services.py
+++ feature_repo/feature_services.py
@@ -1,10 +1,10 @@
 from feast import FeatureService
 
-from feature_repo.traffic_light_features import traffic_light_features
+from feature_repo.traffic_light_features import traffic_light_features_stream
 
 # Define a FeatureService for the traffic light model
 traffic_light_service = FeatureService(
     name="traffic_light_service",
-    features=[traffic_light_features],
+    features=[traffic_light_features_stream],
     description="Service to provide traffic light features"
 )
