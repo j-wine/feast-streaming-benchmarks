b43f17d862e5cd5a88192b236ba5b8c8698db74f
Weinhold, Jacob <jacob.weinhold@stud.th-luebeck.de>
2025-02-03 20:30:06 +0100

    added traffic_light entity type

--- feature_repo/entities.py
+++ feature_repo/entities.py
@@ -1,8 +1,10 @@
-from feast import Entity
+from feast import Entity, ValueType
+from feast.types import Int64
 
 # Define the traffic light entity
 traffic_light = Entity(
     name="traffic_light_id",
     join_keys=["traffic_light_id"],
     description="Unique identifier for each traffic light",
+    value_type=Int64.to_value_type()  # Using the PrimitiveFeastType for INT64
 )
