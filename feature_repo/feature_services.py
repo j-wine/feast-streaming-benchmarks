21bdbe59954835a9f4743a1db11f3b648620a732
Jacob Weinhold <j.f.w@web.de>
2025-02-05 22:05:20 +0100

    # [stable] commented out the stream feature view-> fixed the dill type error in kafka_consumer

--- feature_repo/feature_services.py
+++ feature_repo/feature_services.py
@@ -1,10 +1,10 @@
 from feast import FeatureService
 
-from traffic_light_features import traffic_light_features_stream
-
-# Define a FeatureService for the traffic light model
-traffic_light_service = FeatureService(
-    name="traffic_light_service",
-    features=[traffic_light_features_stream],
-    description="Service to provide traffic light features"
-)
+# from traffic_light_features import traffic_light_features_stream
+#
+# # Define a FeatureService for the traffic light model
+# traffic_light_service = FeatureService(
+#     name="traffic_light_service",
+#     features=[traffic_light_features_stream],
+#     description="Service to provide traffic light features"
+# )
