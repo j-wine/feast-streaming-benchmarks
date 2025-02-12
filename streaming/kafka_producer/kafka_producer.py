26b56d7bfbc756e061de223994709cb8b888f10e
Jacob Weinhold <29459386+j-wine@users.noreply.github.com>
2025-02-12 22:12:31 +0100

    added id upcounting closure

--- streaming/kafka_producer/kafka_producer.py
+++ streaming/kafka_producer/kafka_producer.py
@@ -1,19 +1,26 @@
 import json
+from datetime import datetime, timezone
 from time import sleep
 from kafka import KafkaProducer
 
 KAFKA_TOPIC = "traffic_light_signals"
 KAFKA_BROKER = "broker:9092"
-
+id_counter = 3
 
 def generate_traffic_light_data():
+    global id_counter  # Access the global counter
+    # Get current timestamp in ISO 8601 format (UTC)
+    timestamp = datetime.now(timezone.utc).isoformat()
+    id_counter += 1
     # Simulated data
     return {
         "traffic_light_id": str(id_counter),
         "primary_signal": 3,  # Green
         "secondary_signal": 1,  # Red
         "location": "Dammtor/Theodor-Heuss-Platz",
-        "timestamp": "2024-11-05T12:30:00Z"
+        "event_timestamp": timestamp
+            #"2024-11-05T12:30:00Z"
+
     }
 
 
