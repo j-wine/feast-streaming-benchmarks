4c4a5efd1c8e6b3bcc242bc4dfab0faf66f8a90f
Jacob Weinhold <29459386+j-wine@users.noreply.github.com>
2025-03-03 00:12:03 +0100

    write to offline store

--- streaming/kafka_consumer.py
+++ streaming/kafka_consumer.py
@@ -77,6 +77,9 @@ def persist_to_feast_and_batch(message):
     store.write_to_online_store(feature_view_name="traffic_light_stats", df=df)
     print("store.write_to_online_store(feature_view_name=""traffic_light_stats"", df=, )", df)
 
+    store.write_to_offline_store(feature_view_name="traffic_light_stats", df=df)
+    print("store.write_to_offline_store(feature_view_name=""traffic_light_stats"", df=, )", df)
+
     online_df = store.get_online_features(
         features=[
             "traffic_light_stats:primary_signal",
