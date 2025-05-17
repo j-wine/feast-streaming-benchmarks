8a9281e2f58f3a2424034e97dee93e80faa4c9ad
Jacob Weinhold <29459386+j-wine@users.noreply.github.com>
2025-05-17 13:58:24 +0200

    added sfv 1in_1out

--- feature_repo/traffic_light_features.py
+++ feature_repo/traffic_light_features.py
@@ -7,12 +7,28 @@ from pyspark.sql import DataFrame
 
 from entities import benchmark_entity
 from data_sources import benchmark_stream_source_10_features, benchmark_stream_source_100_features, \
-    benchmark_stream_source_250_features, benchmark_stream_source_50_features
+    benchmark_stream_source_250_features, benchmark_stream_source_50_features, benchmark_stream_source_1_features
 
 
 def generate_feature_schema(num_features: int):
     return [Field(name=f"feature_{i}", dtype=Int64) for i in range(num_features)]
 
+@stream_feature_view(
+    entities=[benchmark_entity],
+    ttl=timedelta(days=140),
+    mode="spark",
+    schema=[
+        Field(name="sum", dtype=Int64),
+    ],
+    timestamp_field="event_timestamp",
+    online=True,
+    source=benchmark_stream_source_1_features,
+)
+def stream_view_1in_1out(df: DataFrame):
+    from pyspark.sql.functions import col
+    from pyspark.sql.types import LongType
+    df = df.withColumn("sum", (col("feature_0")).cast(LongType()))
+    return df.select("benchmark_entity","event_timestamp", "sum")
 
 @stream_feature_view(
     entities=[benchmark_entity],
